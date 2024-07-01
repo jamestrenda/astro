e865bec49467bf007d0ec0b1c109afe00261e337
James Trenda <jamestrenda@gmail.com>
2024-07-01 14:06:44 -0400

    add custom heading block component

---
+++ src/utils/misc.ts
@@ -0,0 +1,9 @@
+import clsx, { type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+/**
+ * A handy utility that makes constructing class names easier.
+ * It also merges tailwind classes.
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}

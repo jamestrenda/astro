b5e668cd214a48e920e82e6a86f60b1054b23716
James Trenda <jamestrenda@gmail.com>
2024-12-13 10:57:15 -0500

    perf: remove 'cn' script

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,5 +1,4 @@
 import { LightbulbIcon, LightbulbOffIcon } from "lucide-react";
-import { cn } from "~/utils/misc";
 import { ThemeFormSchema, type Theme } from "~/utils/theme";
 import { useForm, getFormProps } from "@conform-to/react";
 import { getZodConstraint } from "@conform-to/zod";
@@ -30,23 +29,16 @@ export function ThemeToggle({ initial }: { initial: Theme | undefined }) {
           type="submit"
           name="theme"
           value={isDark ? "light" : "dark"}
-          className={cn(
-            `border-4 border-solid border-black dark:border-background rounded-full bg-foreground dark:bg-background ring-brand text-primary h-9 w-16 grid place-items-center text-white dark:text-background`
-          )}
+          className="border-4 border-solid border-black dark:border-background rounded-full bg-foreground dark:bg-background ring-brand text-primary h-9 w-16 grid place-items-center text-white dark:text-background"
         >
           <div
-            className={cn(
-              "flex w-full",
-              isDark ? "justify-start" : "justify-end"
-            )}
+            className={`flex w-full ${isDark ? "justify-start" : "justify-end"}`}
           >
             {isDark ? (
-              <LightbulbOffIcon
-                className={cn("h-7 w-7 p-1 rounded-full bg-primary")}
-              />
+              <LightbulbOffIcon className="h-7 w-7 p-1 rounded-full bg-primary" />
             ) : (
               <LightbulbIcon
-                className={cn("h-7 w-7 p-1 rounded-full bg-primary")}
+                className="h-7 w-7 p-1 rounded-full bg-primary"
                 strokeWidth={2}
                 key="light"
               />

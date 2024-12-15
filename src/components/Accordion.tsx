98f2948c50b5b075441bb7b2d81d0cfcd1537bdb
James Trenda <jamestrenda@gmail.com>
2024-12-15 15:09:05 -0500

    update accordion and contact form styles

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -15,7 +15,7 @@ interface Props {
 export function Accordion({ data }: Props) {
   return (
     <BaseAccordion
-      type="single"
+      type="multiple"
       className="w-full lg:grid lg:grid-cols-3 lg:gap-8"
     >
       {data?.map((item, i) => {
@@ -23,15 +23,13 @@ export function Accordion({ data }: Props) {
           <AccordionItem
             key={i}
             value={`item-${i.toFixed()}`}
-            className="[&[data-state=closed]>div]:h-0 lg:[&[data-state=closed]>div]:h-auto lg:border-b-0 dark:border-white/5"
+            className="[&[data-state=closed]>div]:h-0 lg:[&[data-state=closed]>div]:h-auto lg:border-b-0 dark:border-white/5 last:border-none"
           >
             <AccordionTrigger className="text-left [&>svg]:h-6 [&>svg]:w-6 md:[&>svg]:h-8 md:[&>svg]:w-8 lg:[&>svg]:hidden lg:cursor-text select-text lg:pointer-events-none">
               {item.title}
             </AccordionTrigger>
             <AccordionContent forceMount>
-              <p className="md:text-lg dark:text-white/50">
-                {item.description}
-              </p>
+              <p className="text-lg dark:text-white/50">{item.description}</p>
             </AccordionContent>
           </AccordionItem>
         );

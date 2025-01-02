b7f04e6d9c45bc5abbbe4a5591f06b533072f991
James Trenda <jamestrenda@gmail.com>
2025-01-02 12:06:46 -0500

    add a dynamnic scale to browser stack

--- src/components/BrowserWindow.tsx
+++ src/components/BrowserWindow.tsx
@@ -1,13 +1,12 @@
 import { cn } from "~/utils/misc";
-import { MotionValue, motion, useTransform } from "motion/react";
+import { motion } from "motion/react";
 import { memo } from "react";
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
   withChrome?: boolean;
   withStack?: boolean;
-  scrollY?: MotionValue<number>;
-  scrollYProgress?: MotionValue<number>;
+  stackCount?: number;
 }
 
 const BrowserWindow = memo(
@@ -16,14 +15,13 @@ const BrowserWindow = memo(
     withChrome = true,
     withStack = true,
     children,
-    scrollY = new MotionValue(),
-    scrollYProgress = new MotionValue(),
+    stackCount = 5,
   }: Props) => {
     return (
       <div className="browser-window md:h-full relative rounded-b-lg">
         <div
           className={cn(
-            "relative z-20 pt-14 bg-black bg-[size:150%] bg-[position:90%] bg-[radial-gradient(circle,hsl(var(--primary-dark)),black_75%)] lg:bg-[radial-gradient(circle,hsl(var(--primary-dark))_10%,black_50%)] dark:bg-[radial-gradient(circle,hsl(var(--primary-dark)),hsl(0 0% 10%)_75%)] rounded-lg max-md:rounded-bl-none w-full h-full p-6 sm:p-16 grid items-center",
+            "relative z-20 pt-14 bg-black bg-[size:150%] bg-[position:90%] bg-[radial-gradient(circle,hsl(0_0%_10%),black_75%)] lg:bg-[radial-gradient(circle,hsl(0_0%_10%)_10%,black_50%)] dark:bg-[radial-gradient(circle,hsl(0_0%_10%),hsl(0 0% 10%)_75%)] rounded-lg max-md:rounded-bl-none w-full h-full p-6 sm:p-16 grid items-center",
             className
           )}
         >
@@ -31,7 +29,6 @@ const BrowserWindow = memo(
             <div
               className={cn(
                 "bg-glass absolute top-0 inset-x-0 z-30 h-14 rounded-t-lg grid items-center"
-                // className
               )}
             >
               <div className="flex items-center justify-between h-full px-4">
@@ -47,8 +44,8 @@ const BrowserWindow = memo(
         </div>
         {withStack && (
           <motion.div className="absolute inset-0">
-            {Array.from({ length: 5 }).map((_, index) => (
-              <Shadow key={index} index={index} scrollY={scrollY} count={5} />
+            {Array.from({ length: stackCount }).map((_, index) => (
+              <Shadow key={index} index={index} total={stackCount} />
             ))}
           </motion.div>
         )}
@@ -59,61 +56,55 @@ const BrowserWindow = memo(
 
 type ShadowProps = {
   index: number;
-  delay?: number;
-  scrollY?: MotionValue<number>;
-  count?: number;
+  total: number;
 };
-const Shadow = memo(
-  ({ index, scrollY = new MotionValue(), count = 1 }: ShadowProps) => {
-    const increment = 12;
-    const bottom = -(index + 1) * increment;
+const Shadow = memo(({ index, total }: ShadowProps) => {
+  const increment = 16;
+  const bottom = -(index + 1) * increment;
 
-    // const bottomOpposite = useMemo(
-    //   () => findOpposite(Math.abs(bottom), count, increment),
-    //   [bottom, count]
-    // );
+  const lightness = calculateLightnessScale(10, 95, 0.9, total, index);
 
-    // const bottomProgress = useTransform(
-    //   scrollY,
-    //   [bottomOpposite - increment, bottomOpposite],
-    //   [bottom, 0]
-    // );
+  return (
+    <motion.div
+      className={`absolute top-auto bottom-0 w-full mx-auto inset-x-0 backdrop-blur-lg rounded-b-lg`}
+      initial={{ bottom: 0 }}
+      animate={{ bottom }}
+      whileInView="animate"
+      viewport={{ once: true }}
+      transition={{
+        duration: 1,
+        delay: 0.05 * index,
+        ease: [0.16, 1, 0.3, 1],
+      }}
+      style={{
+        zIndex: 10 - index,
+        width: `calc(100% - ${32 * (index + 1)}px)`,
+      }}
+    >
+      <div
+        className={cn("bg-black h-24 rounded-lg")}
+        style={
+          {
+            "--lightness": lightness + "%",
+            backgroundColor: `hsla(0 0% var(--lightness) / .9)`,
+          } as React.CSSProperties
+        }
+      />
+    </motion.div>
+  );
+});
 
-    return (
-      <motion.div
-        className={`absolute top-auto bottom-0 w-full mx-auto inset-x-0`}
-        initial={{ bottom: 0 }}
-        animate={{ bottom }}
-        whileInView="animate"
-        viewport={{ once: true }}
-        transition={{
-          duration: 1,
-          delay: 0.05 * index,
-          ease: [0.16, 1, 0.3, 1],
-        }}
-        style={{
-          zIndex: 10 - index,
-          width: `calc(100% - ${32 * (index + 1)}px)`,
-          // bottom: bottomProgress,
-        }}
-      >
-        <div
-          className={cn("bg-black h-24 rounded-lg")}
-          style={
-            {
-              "--lightness": Math.abs(bottom * 1.5) + "%",
-              backgroundColor: `hsl(0 0% var(--lightness))`,
-            } as React.CSSProperties
-          }
-        />
-      </motion.div>
-    );
-  }
-);
+function calculateLightnessScale(
+  min: number,
+  max: number,
+  exponent: number,
+  total: number,
+  index: number
+) {
+  const proportion = index / (total - 1); // Normalize index to a range of [0, 1]
+  const lightness = min + (max - min) * Math.pow(proportion, exponent);
 
-function findOpposite(num: number, count: number, increment: number) {
-  const max = increment * count; // The maximum value in the sequence
-  return max + increment - num;
+  return lightness;
 }
 
 export default BrowserWindow;

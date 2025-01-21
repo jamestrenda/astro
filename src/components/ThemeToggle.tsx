f13eb95d3ff7579adc67af992f64d5017a933ef7
James Trenda <jamestrenda@gmail.com>
2025-01-21 13:31:51 -0500

    animate toggle switch

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,12 +4,19 @@ import { useForm, getFormProps } from "@conform-to/react";
 import { getZodConstraint } from "@conform-to/zod";
 import type { z } from "zod";
 import { useTheme } from "~/utils/hooks/useTheme";
+import { motion } from "motion/react";
+import { cn } from "~/utils/misc";
+
+const spring = {
+  type: "spring",
+  stiffness: 700,
+  damping: 30,
+};
 
 export function ThemeToggle({ initial }: { initial: Theme | undefined }) {
   // TODO: check for isClient
   const { isDark, setTheme } = useTheme(initial);
 
-  // TODO: Astro actions
   const [form] = useForm({
     constraint: getZodConstraint(ThemeFormSchema),
     onSubmit: async (e, { formData }) => {
@@ -31,22 +38,29 @@ export function ThemeToggle({ initial }: { initial: Theme | undefined }) {
           type="submit"
           name="theme"
           value={isDark ? "light" : "dark"}
-          className="border-4 border-solid border-black dark:border-primary rounded-full bg-foreground dark:bg-primary ring-brand text-primary h-9 w-16 grid place-items-center text-white dark:text-background"
+          className={cn(
+            "border-4 border-solid border-black dark:border-primary rounded-full bg-foreground dark:bg-primary ring-brand text-primary h-9 w-16 grid place-items-center text-white dark:text-background",
+            isDark ? "justify-start" : "justify-end"
+          )}
         >
-          <div
-            className={`flex w-full ${isDark ? "justify-start" : "justify-end"}`}
+          <motion.div
+            layout
+            transition={spring}
+            whileHover={{
+              scale: 1.1,
+            }}
           >
             {isDark ? (
-              <LightbulbOffIcon className="h-7 w-7 p-1 rounded-full bg-white" />
+              <LightbulbOffIcon className="h-7 w-7 p-1 rounded-full bg-white shadow-sm " />
             ) : (
               <LightbulbIcon
-                className="h-7 w-7 p-1 rounded-full bg-primary"
+                className="h-7 w-7 p-1 rounded-full bg-primary shadow-sm "
                 strokeWidth={2}
                 key="light"
               />
             )}
             <span className="sr-only">{`Toggle ${isDark ? "light" : "dark"} mode`}</span>
-          </div>
+          </motion.div>
         </button>
       </form>
     </div>

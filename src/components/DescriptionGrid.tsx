684d76507f81e498669a1816fc1355e2b6304e78
James Trenda <jamestrenda@gmail.com>
2025-01-21 17:34:55 -0500

    tweak dark mode styles

--- src/components/DescriptionGrid.tsx
+++ src/components/DescriptionGrid.tsx
@@ -27,7 +27,7 @@ export const DescriptionGrid = ({ header, items }: Props) => {
                   <AccordionItem
                     key={i}
                     value={`item-${i.toFixed()}`}
-                    className="[&[data-state=closed]>div]:h-0 lg:[&[data-state=closed]>div]:h-auto lg:border-b-0 max-lg:last:border-none lg:border lg:border-solid lg:p-8 lg:rounded-lg lg:bg-indigo-50 lg:dark:bg-black/30 dark:border-transparent max-lg:hover:bg-indigo-50  transition hover:delay-0 delay-75 max-lg:-mx-4 max-lg:px-4"
+                    className="[&[data-state=closed]>div]:h-0 [&[data-state=open]]:max-lg:dark:bg-zinc-950 lg:[&[data-state=closed]>div]:h-auto lg:border-b-0 max-lg:last:border-none lg:border lg:border-solid lg:p-8 lg:rounded-lg lg:bg-indigo-50 lg:dark:bg-black/30 dark:border-zinc-950 max-lg:hover:bg-indigo-50 max-lg:hover:dark:bg-zinc-800  transition hover:delay-0 delay-75 max-lg:-mx-4 max-lg:px-4"
                   >
                     {/* TODO: could this implementation cause accessbility issues for larger screens where the button is useless? Could/should I add some kind of aria attributes to address it? */}
                     <AccordionTrigger className="text-left [&>svg]:h-6 [&>svg]:w-6 md:[&>svg]:h-8 md:[&>svg]:w-8 lg:[&>svg]:hidden lg:cursor-text select-text lg:pointer-events-none">
@@ -35,7 +35,7 @@ export const DescriptionGrid = ({ header, items }: Props) => {
                     </AccordionTrigger>
                     <AccordionContent
                       forceMount
-                      className="[&_p]:text-lg dark:text-muted max-lg:pr-12"
+                      className="[&_p]:text-lg dark:text-muted max-lg:pr-12 "
                     >
                       {item.description && (
                         <PortableText portableText={item.description} />

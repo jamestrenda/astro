9107dcfaf6247ba9bb51a5740ba4d6f2db981133
James Trenda <jamestrenda@gmail.com>
2025-01-23 20:00:32 -0500

    minor style tweak

--- src/components/Preview/Exit.tsx
+++ src/components/Preview/Exit.tsx
@@ -13,7 +13,7 @@ export default function ExitPreview() {
       method="POST"
     >
       <button
-        className="bg-indigo-500 p-4 font-bold leading-none text-white transition-colors hover:bg-indigo-600"
+        className="bg-indigo-600 p-4 font-bold leading-none text-white transition-colors hover:bg-indigo-700"
         type="submit"
       >
         Exit Preview Mode

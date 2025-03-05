312f4fa281493dfc2c2a17ccbc70c50383f74284
James Trenda <jamestrenda@gmail.com>
2025-03-05 00:03:22 -0500

    add types

--- src/components/PortableText/Marks.tsx
+++ src/components/PortableText/Marks.tsx
@@ -1,11 +1,18 @@
-import type { PortableTextComponents } from '@portabletext/react';
+import type {
+  PortableTextComponents,
+  PortableTextMarkComponentProps,
+} from '@portabletext/react';
 import type { externalLink } from '~/types/externalLink';
 import type { internalRef } from '~/types/internalRef';
 
 export type Marks = internalRef | externalLink;
 
 export const Marks: PortableTextComponents['marks'] = {
-  externalLink: ({ value, children }) => {
+  externalLink: ({
+    value,
+    children,
+  }: PortableTextMarkComponentProps<externalLink>) => {
+    if (!value) return null;
     return (
       <a
         href={value.url ?? '#'}
@@ -16,16 +23,25 @@ export const Marks: PortableTextComponents['marks'] = {
       </a>
     );
   },
-  internalRef: ({ value, children }) => {
+  internalRef: ({
+    value,
+    children,
+  }: PortableTextMarkComponentProps<internalRef>) => {
+    if (!value) return null;
     return (
       <a href={`/${value.slug}`} className="text-blue-500">
         {children}
       </a>
     );
   },
-  code: ({ value, children }) => {
+  code: ({
+    value,
+    children,
+  }: PortableTextMarkComponentProps<{ _type: 'code' }>) => {
     return (
-      <code className="rounded-md bg-background dark:bg-zinc-950 p-1 text-muted dark:text-white/50 text-sm font-mono not-prose pointer-events-auto">{children}</code>
+      <code className="not-prose pointer-events-auto rounded-md bg-background p-1 font-mono text-sm text-muted dark:bg-zinc-950 dark:text-white/50">
+        {children}
+      </code>
     );
   },
 };
